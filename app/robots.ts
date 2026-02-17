a2808605b31bd8d64bcf1659c9de06901783f5b8
Billionaire-coder <bbaaabbuurraaoo@gmail.com>
2026-02-17 20:34:23 +0530

    Fix robots.txt sitemap URL to use correct domain

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
             userAgent: '*',
             allow: '/',
         },
-        sitemap: 'https://pdftoolkit.com/sitemap.xml',
+        sitemap: 'https://pdftoolskit.vercel.app/sitemap.xml',
     };
 }
